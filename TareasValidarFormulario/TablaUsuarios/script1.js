50880dcdd82f29941c8e1c86c5ecfe81ff6d0721
MarioJ-Educarex <mjuareza01@educarex.es>
2023-10-22 19:26:57 +0200

    Ejercicio formulario login usuario

---
+++ TareasValidarFormulario/TablaUsuarios/script1.js
@@ -0,0 +1,81 @@
+let accion = document.getElementById("formLogin");
+
+let bool = true;
+
+accion.onsubmit = function () {
+    console.log("Entro en accion");
+
+    bool = true;
+
+    let user = document.getElementById("uname1").value;
+    let smUser = document.getElementById("smUsers");
+
+    let contrasena = document.getElementById("pwd1").value;
+    let smContrasena = document.getElementById("smPassword");
+
+    let repContrasena = document.getElementById("pwd2").value;
+    let smRepContrasena = document.getElementById("smPassword2");
+
+
+    validacionUser(user, smUser);
+    validacionPswd(contrasena, smContrasena);
+    validacionPswd2(repContrasena, contrasena, smRepContrasena);
+
+
+    return bool;
+
+}
+
+//Función para validar el campo usuario
+function validacionUser(text, sm) {
+    console.log("Usuario: " + text + " Longitud: " + text.length);
+
+    if (text == "") {
+        sm.innerHTML = "* Campo obligatorio.";
+        bool = false;
+    }
+    // else if (text.length < 8) {
+    //     sm.innerHTML = "* Mínimo 8 caracteres.";
+    //     bool = false;
+    // } else if (!/[A-Z]/.test(text) || !/\d/.test(text)) {
+    //     sm.innerHTML = "* Debe contener al menos una letra mayúscula y un número.";
+    //     bool = false;
+    // } 
+    else {
+        sm.innerHTML = "";
+    }
+}
+
+//Función para validar el campo contraseña
+function validacionPswd(text, sm) {
+    console.log("Contraseña: " + text + " Longitud: " + text.length);
+    // const expresionRegular = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\$).{8,}$/;
+    if (text == "") {
+        sm.innerHTML = "* Campo obligatorio.";
+        bool = false;
+    } else if (text.length < 8) {
+        sm.innerHTML = "* Mínimo 8 caracteres.";
+        bool = false;
+    } else if (!/[A-Z]/.test(text) || !/[a-z]/.test(text) || !/\d/.test(text) || !text.includes('$')) {
+        sm.innerHTML = "* Debe contener al menos una letra mayúscula, una minúscula, un número y el símbolo $.";
+        bool = false;
+    } else {
+        sm.innerHTML = "";
+    }
+}
+
+//Función para validar el campo repite contraseña
+function validacionPswd2(text, passwd, sm) {
+    console.log("Contraseña: " + text + " Longitud: " + text.length);
+
+    if (text == "") {
+        sm.innerHTML = "* Campo obligatorio.";
+        bool = false;
+    } else if (text !== passwd) {
+        sm.innerHTML = "* Contraseña distinta.";
+        bool = false;
+    } else {
+        sm.innerHTML = "";
+    }
+}
+
