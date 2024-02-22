dbb8d83c7fee06837146ae580d5b001c129720f7
MarioJ-Educarex <mjuareza01@educarex.es>
2024-02-22 10:34:42 +0100

    Reactivo 1

--- Angular/ProyectoBase/srcReactivo1/app/app.module.ts
+++ Angular/ProyectoBase/srcReactivo1/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Formulario1Component } from './formulario1/formulario1.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Formulario1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
