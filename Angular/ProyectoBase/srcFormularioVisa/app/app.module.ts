930195c4e00d5506418c3823d4cef6e9148a2185
MarioJ-Educarex <mjuareza01@educarex.es>
2024-02-22 12:23:06 +0100

    Cambio nuevo proyecto

--- Angular/ProyectoBase/srcFormularioVisa/app/app.module.ts
+++ Angular/ProyectoBase/srcFormularioVisa/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
